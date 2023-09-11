bf127ca5901ee13373f90f0be4ab97c5855bf0de
Sxmuel <141831574+Sxmuel999@users.noreply.github.com>
2023-09-11 11:43:27 -0300

    Exercícios 11/09/23
    FOR
    SWITCH
    CASE
    WHILE

---
+++ Numeros pares no intervalo/Numeros pares no intervalo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros_pares_no_intervalo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 1 , valor2 = 2;
+
+            Console.WriteLine("Digite o primeiro valor");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor");
+            valor2 = int.Parse(Console.ReadLine());
+
+            for (int cont = valor1; cont <valor2; cont++)
+            {
+                Console.WriteLine("for " + cont);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
